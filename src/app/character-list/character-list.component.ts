9b580243d828370c734bbb6282cc2f64bcd470a0
valerioprds <v.paradiso89@gmail.com>
2023-10-27 09:46:14 +0200

    basic html structure

--- src/app/character-list/character-list.component.ts
+++ src/app/character-list/character-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CharacterService } from 'src/services/character.service';
 
 @Component({
   selector: 'app-character-list',
   templateUrl: './character-list.component.html',
-  styleUrls: ['./character-list.component.css']
+  styleUrls: ['./character-list.component.css'],
 })
-export class CharacterListComponent {
+export class CharacterListComponent implements OnInit {
+  characters: any[] = [];
 
+  constructor(private characterService: CharacterService) {}
+
+  ngOnInit() {
+    this.characterService.getCharacters().subscribe(
+      (data: any) => {
+        this.characters = data['results']; // Access the 'results' property directly
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+      }
+    );
+  }
 }
